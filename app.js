2c89246ffd15f8c20b126050bf0fcff2ba583d75
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-03 20:13:55 +0300

    added initial command parsing... next up parsing additinal commands

--- app.js
+++ app.js
@@ -3,4 +3,12 @@ const notes = require('./notes');
 const validator = require('validator');
 const chalk = require('chalk');
 
-console.log(chalk.green.bold.underline('TEST!'));
+const command = process.argv[2];
+
+switch (command) {
+  case '-a': console.log('We are adding!'); break;
+  case '-r': console.log('We are reading!'); break;
+  case '-w': console.log('We are writing!'); break;
+  case '-d': console.log('We are deleting!'); break;
+  default: console.log('no command / invalid command provided!!!'); break;
+}
