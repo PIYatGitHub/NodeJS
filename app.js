b35dc876d62cdbed20008484b5cf69400a8fc8c1
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-03 22:18:54 +0300

    small refactoring - app.js is a lot cleaner now - created a new file called it commands.js - it holds the yargs configs

--- app.js
+++ app.js
@@ -1,50 +1,7 @@
 'use strict';
+
 const chalk = require('chalk');
 const yargs = require('yargs');
-const notes = require('./notes');
-
-yargs.command({
-  command: 'add',
-  describe: 'add your note',
-  builder:{
-    title: {
-        describe:'Note title',
-        demandOption: true,
-        type: 'string'
-    },
-    body: {
-      describe:'Note body',
-      demandOption: true,
-      type: 'string'
-    }
-  },
-  handler: function (argv) {
-    notes.addNote(argv.title, argv.body);
-  }
-});
-
-yargs.command({
-  command: 'remove',
-  describe: 'remove your note',
-  handler: function () {
-    console.log('fire up that remove!');
-  }
-});
-
-yargs.command({
-  command: 'list',
-  describe: 'list all your notes',
-  handler: function () {
-    console.log('fire up that list!');
-  }
-});
-
-yargs.command({
-  command: 'read',
-  describe: 'read a note',
-  handler: function () {
-    console.log('fire up that read!');
-  }
-});
+require('./commands');
 
 yargs.parse();
